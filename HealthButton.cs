0d15ddaaa01b297779ba5d268c3953ba136b5044
GeorgiiProjects <89801261+GeorgiiProjects@users.noreply.github.com>
2022-01-20 14:16:24 +0300

    Update

--- HealthButton.cs
+++ HealthButton.cs
@@ -4,6 +4,7 @@ public class HealthButton : MonoBehaviour
 {
     [SerializeField] private Player _player;
     [SerializeField] private float _healing;
+
     public void RestoreHealth()
     {
         _player.RestoreHealth(_healing);
